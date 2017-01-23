8afb4eadb852bf24261809ed43287bcbad9c37b8
Igor Lapin <raynor73@gmail.com>
2017-01-23 11:17:36 +0300

    Sync

--- app/src/main/java/org/ilapin/transactionviewer/currency/CurrencyCalculator.java
+++ app/src/main/java/org/ilapin/transactionviewer/currency/CurrencyCalculator.java
@@ -1,8 +1,42 @@
 package org.ilapin.transactionviewer.currency;
 
+import com.google.common.graph.MutableValueGraph;
+import com.google.common.graph.ValueGraphBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import java8.util.stream.StreamSupport;
+
 public class CurrencyCalculator {
 
+	private static final String ROOT_NODE_NAME = "_root_";
+	private static final float ROOT_NODE_CONVERSION_RATE = 1;
+
+	private final MutableValueGraph<String, Float> mCurrenciesGraph = ValueGraphBuilder.directed().build();
+
+	public CurrencyCalculator() {
+		mCurrenciesGraph.addNode(ROOT_NODE_NAME);
+	}
+
+	public void addConversion(final String from, final String to, final float rate) {
+		if (!mCurrenciesGraph.nodes().contains(from)) {
+			mCurrenciesGraph.addNode(from);
+			mCurrenciesGraph.putEdgeValue(ROOT_NODE_NAME, from, ROOT_NODE_CONVERSION_RATE);
+		}
+		mCurrenciesGraph.addNode(to);
+		mCurrenciesGraph.putEdgeValue(from, to, rate);
+	}
+
 	public float calculate(final String from, final String to, final float amount) throws NoConversionException {
+		final Set<String> nodes = mCurrenciesGraph.nodes();
+		if (!nodes.contains(from) || !nodes.contains(to)) {
+			throw new NoConversionException();
+		}
+
+		final List<Float> rates = new ArrayList<>();
+
 		return amount;
 	}
 }
